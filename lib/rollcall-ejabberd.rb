b2f70b98c6beff89fecf949112a29bffe64b0774
Matt Zukowski <matt@zukowski.ca>
2011-06-24 17:16:57 -0400

    XMPP domain can now be specified via config.ejabberd.domain

--- lib/rollcall-ejabberd.rb
+++ lib/rollcall-ejabberd.rb
@@ -5,7 +5,13 @@ module RollcallEjabberd
     initializer 'ejabberd.initialize' do |app|
       if config.ejabberd.mod_rest_url
         RollcallEjabberd::MOD_REST_URL = config.ejabberd.mod_rest_url
-
+        
+        if config.ejabberd.domain
+          RollcallEjabberd::DOMAIN = config.ejabberd.domain
+        else
+          RollcallEjabberd::DOMAIN = 'localhost'
+        end
+        
         require 'account_callbacks'
       end
     end
